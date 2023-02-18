9591581bbcd870c6b0b7d1db024ed96bab00c2d3
sechshelme <m_burkhard@gmx.ch>
2023-02-18 18:04:34 +0100

    vbox

--- zetcode.com_gui_gtk2_introduction/40-layout_management/20-GTKAlignment_2/Project1.pas
+++ zetcode.com_gui_gtk2_introduction/40-layout_management/20-GTKAlignment_2/Project1.pas
@@ -1,15 +1,17 @@
 program Project1;
 
 uses
+  heaptrc,
+  sysutils,
   GLib2,
   Gdk2,
   Gtk2;
 
 // function atoll(const nptr: pChar): int64;        cdecl; external 'libc';
 
-  procedure press(widget: PGtkWidget; Caption: PChar); cdecl;
+  procedure press(widget: PGtkWidget; Caption: PString); cdecl;
   begin
-    WriteLn('Cklicked: ', Caption);
+    WriteLn('Cklicked: ', Caption^);
   end;
 
   procedure main;
@@ -22,7 +24,7 @@ uses
   var
     widget: array[0..max] of record
     allign, button: PGtkWidget;
-    pc: array [0..20] of char;
+    Caption: String;
     end;
     align1, button1, align2, button2, align3, button3: PGtkWidget;
   begin
@@ -39,31 +41,32 @@ uses
 
     for i := 0 to max do begin
       str(i, s);
-      widget[i].pc := ('Button' + s);
+      widget[i].Caption := 'Button' + s;
       widget[i].allign := gtk_alignment_new(i / max, 0, 0, 0);
-      widget[i].button := gtk_button_new_with_label(widget[i].pc);
+//      widget[i].button := gtk_button_new_with_label(PChar(widget[i].Caption));
+      widget[i].button := gtk_button_new_with_label(@widget[i].Caption[1]);
       gtk_container_add(GTK_CONTAINER(widget[i].allign), widget[i].button);
       gtk_box_pack_start(GTK_BOX(vbox), widget[i].allign, False, False, 0);
-      g_signal_connect(G_OBJECT(widget[i].button), 'clicked', G_CALLBACK(@press), @widget[i].pc);
+      g_signal_connect(G_OBJECT(widget[i].button), 'clicked', G_CALLBACK(@press), @widget[i].Caption);
     end;
 
     align1 := gtk_alignment_new(0, 0, 0, 0);
     button1 := gtk_button_new_with_label('Button1');
     gtk_container_add(GTK_CONTAINER(align1), button1);
     gtk_box_pack_start(GTK_BOX(vbox), align1, False, False, 0);
-    g_signal_connect(G_OBJECT(button1), 'clicked', G_CALLBACK(@press), PChar('Button1'));
+//    g_signal_connect(G_OBJECT(button1), 'clicked', G_CALLBACK(@press), PChar('Button1'));
 
     align2 := gtk_alignment_new(0.5, 0, 0, 0);
     button2 := gtk_button_new_with_label('Button2');
     gtk_container_add(GTK_CONTAINER(align2), button2);
     gtk_box_pack_start(GTK_BOX(vbox), align2, False, False, 0);
-    g_signal_connect(G_OBJECT(button2), 'clicked', G_CALLBACK(@press), PChar('Button2'));
+//    g_signal_connect(G_OBJECT(button2), 'clicked', G_CALLBACK(@press), PChar('Button2'));
 
     align3 := gtk_alignment_new(1.0, 0, 0, 0);
     button3 := gtk_button_new_with_label('Button3');
     gtk_container_add(GTK_CONTAINER(align3), button3);
     gtk_box_pack_start(GTK_BOX(vbox), align3, False, False, 0);
-    g_signal_connect(G_OBJECT(button3), 'clicked', G_CALLBACK(@press), PChar('Button3'));
+//    g_signal_connect(G_OBJECT(button3), 'clicked', G_CALLBACK(@press), PChar('Button3'));
 
 
     gtk_container_add(GTK_CONTAINER(Window), vbox);
--- zetcode.com_gui_gtk2_introduction/40-layout_management/30-GTKVBox/Project1.pas
+++ zetcode.com_gui_gtk2_introduction/40-layout_management/30-GTKVBox/Project1.pas
@@ -12,7 +12,7 @@ uses
 
   procedure main;
   var
-    Window, align1, button1, button2, vbox, align2, align3, button3: PGtkWidget;
+    Window, vbox, button1, button2, button3, button4, button5: PGtkWidget;
   begin
     GTK_Init(@argc, @argv);
 
@@ -24,27 +24,27 @@ uses
     gtk_container_set_border_width(GTK_CONTAINER(Window), 5);
 
     vbox := gtk_vbox_new(False, 0);
+    gtk_container_add(GTK_CONTAINER(Window), vbox);
 
-    align1 := gtk_alignment_new(0, 0, 0, 0);
-    button1 := gtk_button_new_with_label('Button1');
-    gtk_container_add(GTK_CONTAINER(align1), button1);
-    gtk_box_pack_start(GTK_BOX(vbox), align1, False, False, 0);
+    button1 := gtk_button_new_with_label('Settings');
+    gtk_box_pack_start(GTK_BOX(vbox), button1, False, False, 0);
     g_signal_connect(G_OBJECT(button1), 'clicked', G_CALLBACK(@press), PChar('Button1'));
 
-    align2 := gtk_alignment_new(0.5, 0, 0, 0);
-    button2 := gtk_button_new_with_label('Button2');
-    gtk_container_add(GTK_CONTAINER(align2), button2);
-    gtk_box_pack_start(GTK_BOX(vbox), align2, False, False, 0);
+    button2 := gtk_button_new_with_label('Accounts');
+    gtk_box_pack_start(GTK_BOX(vbox), button2, False, False, 0);
     g_signal_connect(G_OBJECT(button2), 'clicked', G_CALLBACK(@press), PChar('Button2'));
 
-    align3 := gtk_alignment_new(1.0, 0, 0, 0);
-    button3 := gtk_button_new_with_label('Button3');
-    gtk_container_add(GTK_CONTAINER(align3), button3);
-    gtk_box_pack_start(GTK_BOX(vbox), align3, False, False, 0);
+    button3 := gtk_button_new_with_label('Loans');
+    gtk_box_pack_start(GTK_BOX(vbox), button3, False, False, 0);
     g_signal_connect(G_OBJECT(button3), 'clicked', G_CALLBACK(@press), PChar('Button3'));
 
+    button4 := gtk_button_new_with_label('cash');
+    gtk_box_pack_start(GTK_BOX(vbox), button4, False, False, 0);
+    g_signal_connect(G_OBJECT(button4), 'clicked', G_CALLBACK(@press), PChar('Button4'));
 
-    gtk_container_add(GTK_CONTAINER(Window), vbox);
+    button5 := gtk_button_new_with_label('depts');
+    gtk_box_pack_start(GTK_BOX(vbox), button5, False, False, 0);
+    g_signal_connect(G_OBJECT(button5), 'clicked', G_CALLBACK(@press), PChar('Button5'));
 
     gtk_widget_show_all(window);
 
